c9aa49f0342cc3cc01afc60b85fcaba9cf071e74
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-30 11:59:30 +0530

    basic product item added

--- lib/presentation/screens/products/products_screen.dart
+++ lib/presentation/screens/products/products_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:price_checker/domain/product/models/product.dart';
+import 'package:price_checker/domain/product/value_objects/product_name.dart';
 
 class ProductScreen extends StatelessWidget {
   static Widget generateRoute() {
@@ -7,9 +9,37 @@ class ProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Product> products = [
+      Product(name: ProductName("Potato")),
+      Product(name: ProductName("Tomato")),
+      Product(name: ProductName("Belt")),
+      Product(name: ProductName("Belt")),
+      Product(name: ProductName("Belt")),
+      Product(name: ProductName("Belt")),
+    ];
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
       appBar: AppBar(
         title: Text("Products"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemBuilder: (_, index) {
+              final product = products[index];
+              return Card(
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Icon(Icons.local_mall),
+                  ),
+                  title: Text(product.name.value),
+                ),
+              );
+            },
+            itemCount: products.length),
       ),
     );
   }
