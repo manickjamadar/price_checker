90561767d0c3a7db4f57bf5dd574c3fb8ee5f982
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-30 14:39:14 +0530

    basic input dialog created

---
+++ lib/presentation/core/widgets/input_dialog.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class InputDialog extends StatefulWidget {
+  final String title;
+  final String hint;
+
+  const InputDialog({Key key, @required this.title, @required this.hint})
+      : super(key: key);
+
+  @override
+  _InputDialogState createState() => _InputDialogState();
+}
+
+class _InputDialogState extends State<InputDialog> {
+  TextEditingController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(widget.title),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextField(
+            controller: _controller,
+            autofocus: true,
+            decoration: InputDecoration(labelText: widget.hint),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              FlatButton(
+                child: Text("Cancel"),
+                onPressed: () => _onCancel(),
+              ),
+              FlatButton(
+                child: Text("Ok"),
+                onPressed: () => _onDone(),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  void _onCancel() {
+    Navigator.pop(context);
+  }
+
+  void _onDone() {
+    if (_controller.text.isEmpty) {
+      return;
+    }
+    Navigator.pop(context, _controller.text);
+  }
+}
