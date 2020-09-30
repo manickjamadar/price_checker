55306c5fc4c15d2b3bcd0462337ef3550359e4e5
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-30 12:45:21 +0530

    product cubit registered

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:price_checker/application/cubit/product_cubit.dart';
 import 'package:price_checker/presentation/core/app.dart';
 
 void main() {
-  runApp(App());
+  runApp(MultiBlocProvider(
+      providers: [BlocProvider(create: (_) => ProductCubit()..init())],
+      child: App()));
 }
