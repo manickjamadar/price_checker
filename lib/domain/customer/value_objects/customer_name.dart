eb6502db00acdc63e5ae2c3e11ec898bf3b0aecf
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-30 12:02:51 +0530

    customer name created

---
+++ lib/domain/customer/value_objects/customer_name.dart
@@ -0,0 +1,8 @@
+import 'package:price_checker/domain/core/value_object/value_object.dart';
+
+class CustomerName extends ValueObject<String> {
+  const CustomerName(String value) : super(value);
+
+  @override
+  bool get isValid => value.isNotEmpty;
+}
