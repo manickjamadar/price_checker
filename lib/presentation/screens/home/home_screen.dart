600cb25f254161411f7d5054d2345d842b0e8166
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-10-01 18:55:34 +0530

    on delete logic added in customer home screen

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:price_checker/application/customer/customer_cubit.dart';
 import 'package:price_checker/application/product/product_cubit.dart';
 import 'package:price_checker/domain/customer/models/customer.dart';
+import 'package:price_checker/presentation/core/helpers/deletor.dart';
 import 'package:price_checker/presentation/screens/customer_form/customer_form_screen.dart';
 import 'package:price_checker/presentation/screens/products/products_screen.dart';
 
@@ -61,7 +62,8 @@ class HomeScreen extends StatelessWidget {
           ),
           title: Text(customer.name.value),
           subtitle: Text("${customer.activeProducts.length} Products"),
-          trailing: buildMoreButton(context, customer),
+          trailing:
+              Builder(builder: (context) => buildMoreButton(context, customer)),
         );
       },
       itemCount: customers.length,
@@ -76,7 +78,7 @@ class HomeScreen extends StatelessWidget {
         if (index == 0) {
           _onEdit(context, customer);
         } else if (index == 1) {
-          _onDelete(context);
+          _onDelete(context, customer);
         }
       },
       itemBuilder: (_) => menuItems
@@ -98,7 +100,31 @@ class HomeScreen extends StatelessWidget {
                 CustomerFormScreen.generateRoute(context, customer: customer)));
   }
 
-  void _onDelete(BuildContext context) {}
+  void _onDelete(BuildContext context, Customer customer) async {
+    final deletor = Deletor(customer.id);
+    final duration = Duration(seconds: 3);
+    BlocProvider.of<CustomerCubit>(context).delete(deletor);
+    Scaffold.of(context).removeCurrentSnackBar();
+    Scaffold.of(context).showSnackBar(SnackBar(
+      duration: duration,
+      content: Text(
+        "${customer.name.value} deleted successfully",
+        style: TextStyle(color: Colors.white),
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+      ),
+      backgroundColor: Colors.green,
+      action: SnackBarAction(
+        label: "Undo",
+        onPressed: () => deletor.undo(),
+        textColor: Colors.white,
+      ),
+    ));
+    await Future.delayed(duration);
+    if (deletor.canPerform) {
+      deletor.delete();
+    }
+  }
 
   void _onAddCustomer(BuildContext context) {
     Navigator.push(
