b3f91897c90f729663bfe4ab12a04e93fb8fb8e0
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-10-02 16:37:28 +0530

    locked portrait only

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
 import "package:hive_flutter/hive_flutter.dart";
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,6 +10,10 @@ import 'package:price_checker/service_locator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   await Hive.initFlutter();
   await initLocator();
   runApp(MultiBlocProvider(providers: [
