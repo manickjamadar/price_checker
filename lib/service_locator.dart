234b78beca411f7c64031ae92f58db7a2227c110
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-10-02 19:39:15 +0530

    register customer facade and data source

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -2,7 +2,11 @@ import 'package:get_it/get_it.dart';
 import 'package:hive/hive.dart';
 import 'package:price_checker/application/customer/customer_cubit.dart';
 import 'package:price_checker/application/product/product_cubit.dart';
+import 'package:price_checker/domain/customer/facade/i_customer_facade.dart';
 import 'package:price_checker/domain/product/facade/product_facade.dart';
+import 'package:price_checker/infrastructure/customer/data_source/hive_customer_data_source.dart';
+import 'package:price_checker/infrastructure/customer/data_source/i_customer_data_source.dart';
+import 'package:price_checker/infrastructure/customer/facade/customer_facade.dart';
 import 'package:price_checker/infrastructure/product/data_source/hive_product_data_source.dart';
 import 'package:price_checker/infrastructure/product/data_source/i_product_data_source.dart';
 import 'package:price_checker/infrastructure/product/facade/product_facade.dart';
@@ -16,7 +20,8 @@ Future<void> initLocator() async {
 }
 
 void _initApplication() {
-  locator.registerFactory(() => CustomerCubit());
+  locator.registerFactory(
+      () => CustomerCubit(customerFacade: locator<ICustomerFacade>()));
   locator.registerFactory(
       () => ProductCubit(productFacade: locator<IProductFacade>()));
 }
@@ -24,10 +29,15 @@ void _initApplication() {
 void _initFacade() {
   locator.registerLazySingleton<IProductFacade>(
       () => ProductFacade(dataSource: locator<IProductDataSource>()));
+  locator.registerLazySingleton<ICustomerFacade>(
+      () => CustomerFacade(dataSource: locator<ICustomerDataSource>()));
 }
 
 Future<void> _initDataSource() async {
   final productBox = await Hive.openBox<String>("product");
+  final customerBox = await Hive.openBox<String>("customer");
   locator.registerLazySingleton<IProductDataSource>(
       () => HiveProductDataSource(productBox));
+  locator.registerLazySingleton<ICustomerDataSource>(
+      () => HiveCustomerDataSource(customerBox));
 }
