1d29f2035a8a245ce4946100987fb508951ef0aa
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-10-02 16:19:06 +0530

    hive and service locator initialized properly in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import "package:hive_flutter/hive_flutter.dart";
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:price_checker/application/customer/customer_cubit.dart';
 import 'package:price_checker/application/product/product_cubit.dart';
 import 'package:price_checker/presentation/core/app.dart';
 import 'package:price_checker/service_locator.dart';
 
-void main() {
-  initLocator();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await initLocator();
   runApp(MultiBlocProvider(providers: [
     BlocProvider(
       lazy: false,
