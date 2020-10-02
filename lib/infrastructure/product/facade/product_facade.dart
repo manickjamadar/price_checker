dfad8db31bb210bd54eb94c410a0576be9493a12
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-10-02 16:07:42 +0530

    real product facade created

--- lib/infrastructure/product/facade/product_facade.dart
+++ lib/infrastructure/product/facade/product_facade.dart
@@ -1,38 +1,53 @@
+import 'package:flutter/foundation.dart';
 import 'package:price_checker/domain/core/unique_id.dart';
 import 'package:dartz/dartz.dart';
 import 'package:price_checker/domain/product/facade/product_facade.dart';
 import 'package:price_checker/domain/product/models/product.dart';
-import 'package:price_checker/domain/product/value_objects/product_name.dart';
+import 'package:price_checker/infrastructure/product/data_source/i_product_data_source.dart';
+import 'package:price_checker/infrastructure/product/entity/product_entity.dart';
 
 class ProductFacade extends IProductFacade {
+  final IProductDataSource dataSource;
+
+  ProductFacade({@required this.dataSource});
   @override
-  Future<Either<Error, Unit>> create(Product product) {
-    // TODO: implement create
-    throw UnimplementedError();
+  Future<Either<Error, Unit>> create(Product product) async {
+    try {
+      await dataSource.create(ProductEntity.fromModel(product));
+      return Right(unit);
+    } catch (_) {
+      return Left(Error());
+    }
   }
 
   @override
-  Future<Either<Error, Product>> delete(UniqueId id) {
-    // TODO: implement delete
-    throw UnimplementedError();
+  Future<Either<Error, Product>> delete(UniqueId id) async {
+    try {
+      final entity = await dataSource.delete(id.value);
+      return Right(entity?.toModel());
+    } catch (_) {
+      return Left(Error());
+    }
   }
 
   @override
   Future<Either<Error, List<Product>>> findAll() async {
-    // TODO: implement findAll
-    final products = [
-      Product(id: UniqueId(), name: ProductName("Potato")),
-      Product(id: UniqueId(), name: ProductName("Tomato")),
-      Product(id: UniqueId(), name: ProductName("Rice")),
-      Product(id: UniqueId(), name: ProductName("Fish")),
-      Product(id: UniqueId(), name: ProductName("Meat")),
-    ];
-    return Right(products);
+    try {
+      final entityList = await dataSource.findAll();
+      final modelList = entityList.map((entity) => entity.toModel()).toList();
+      return Right(modelList);
+    } catch (_) {
+      return Left(Error());
+    }
   }
 
   @override
-  Future<Either<Error, Unit>> update(Product product) {
-    // TODO: implement update
-    throw UnimplementedError();
+  Future<Either<Error, Unit>> update(Product product) async {
+    try {
+      await dataSource.update(ProductEntity.fromModel(product));
+      return Right(unit);
+    } catch (_) {
+      return Left(Error());
+    }
   }
 }
