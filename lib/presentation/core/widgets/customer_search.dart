62f29e68e07115cfb9aac14ac28140546bf0757b
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-10-02 12:00:42 +0530

    select and delete and edit added in customer search

--- lib/presentation/core/widgets/customer_search.dart
+++ lib/presentation/core/widgets/customer_search.dart
@@ -3,7 +3,10 @@ import 'package:price_checker/domain/customer/models/customer.dart';
 import 'package:price_checker/presentation/core/widgets/item_search.dart';
 
 class CustomerSearch extends ItemSearch<Customer> {
-  CustomerSearch(List<Customer> items) : super(items);
+  final void Function(Customer customer) onDelete;
+  final void Function(Customer customer) onEdit;
+  CustomerSearch(List<Customer> items, {this.onDelete, this.onEdit})
+      : super(items);
   @override
   Widget build(BuildContext context, String query, List<Customer> customers) {
     if (customers.isEmpty) {
@@ -13,17 +16,43 @@ class CustomerSearch extends ItemSearch<Customer> {
       itemBuilder: (_, index) {
         final customer = customers[index];
         return ListTile(
-          leading: CircleAvatar(
-            child: Icon(Icons.person),
-          ),
-          title: Text(customer.name.value),
-        );
+            onTap: () => close(context, customer),
+            leading: CircleAvatar(
+              child: Icon(Icons.person),
+            ),
+            title: Text(customer.name.value),
+            subtitle: Text("${customer.activeProducts.length} Products"),
+            trailing: buildMoreButton(context, customer));
       },
       itemCount: customers.length,
     );
   }
 
   Widget buildEmpty() => Center(child: Text("No Customer Found"));
+
+  Widget buildMoreButton(BuildContext context, Customer customer) {
+    final menuItems = ["Edit", "Delete"];
+    return PopupMenuButton<int>(
+      icon: Icon(Icons.more_vert),
+      onSelected: (index) {
+        FocusScope.of(context).unfocus();
+        close(context, null);
+        if (index == 0 && onEdit != null) {
+          onEdit(customer);
+        } else if (index == 1 && onDelete != null) {
+          onDelete(customer);
+        }
+      },
+      itemBuilder: (_) => menuItems
+          .asMap()
+          .keys
+          .map((key) => PopupMenuItem<int>(
+                child: Text(menuItems[key]),
+                value: key,
+              ))
+          .toList(),
+    );
+  }
 
   @override
   bool filter(String query, Customer customer) {
