134bb5829fc83bd1891eb038c3fa1bf746091064
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-10-02 16:09:37 +0530

    product facade and data source registered in service locator

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,14 +1,18 @@
 import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
 import 'package:price_checker/application/customer/customer_cubit.dart';
 import 'package:price_checker/application/product/product_cubit.dart';
 import 'package:price_checker/domain/product/facade/product_facade.dart';
+import 'package:price_checker/infrastructure/product/data_source/hive_product_data_source.dart';
+import 'package:price_checker/infrastructure/product/data_source/i_product_data_source.dart';
 import 'package:price_checker/infrastructure/product/facade/product_facade.dart';
 
 final locator = GetIt.instance;
 
-void initLocator() {
+Future<void> initLocator() async {
   _initApplication();
   _initFacade();
+  await _initDataSource();
 }
 
 void _initApplication() {
@@ -18,5 +22,12 @@ void _initApplication() {
 }
 
 void _initFacade() {
-  locator.registerLazySingleton<IProductFacade>(() => ProductFacade());
+  locator.registerLazySingleton<IProductFacade>(
+      () => ProductFacade(dataSource: locator<IProductDataSource>()));
+}
+
+Future<void> _initDataSource() async {
+  final productBox = await Hive.openBox<String>("product");
+  locator.registerLazySingleton<IProductDataSource>(
+      () => HiveProductDataSource(productBox));
 }
