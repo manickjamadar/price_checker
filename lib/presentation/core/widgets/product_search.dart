4a64ff7a1cb0a58da8d6aa98bb20ccc417301a60
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-10-02 11:09:21 +0530

    product search created

---
+++ lib/presentation/core/widgets/product_search.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:price_checker/domain/product/models/product.dart';
+import 'package:price_checker/presentation/core/widgets/item_search.dart';
+// import 'package:price_checker/presentation/core/widgets/item_search.dart';
+
+class ProductSearch extends ItemSearch<Product> {
+  final List<Product> products;
+  ProductSearch(this.products) : super(products);
+
+  @override
+  Widget build(BuildContext context, String query, List<Product> items) {
+    if (items.isEmpty) {
+      return Center(child: Text("No Product found"));
+    }
+    return ListView.builder(
+        itemBuilder: (_, index) {
+          final product = items[index];
+          return ListTile(
+            onTap: () {
+              close(context, product);
+            },
+            leading: CircleAvatar(
+              child: Icon(Icons.local_mall),
+            ),
+            title: Text(product.name.value),
+            trailing: Icon(Icons.radio_button_unchecked),
+          );
+        },
+        itemCount: items.length);
+  }
+
+  @override
+  bool filter(String query, Product item) {
+    print("product $query");
+    final b = item.name.value.toLowerCase().contains(query.toLowerCase());
+    print(b);
+    return b;
+  }
+}
