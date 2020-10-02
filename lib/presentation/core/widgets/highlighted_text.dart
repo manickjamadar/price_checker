3f610357ec99de0302c8d14d843ac2f0636d9fca
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-10-02 13:54:40 +0530

    highlighted text widget created

---
+++ lib/presentation/core/widgets/highlighted_text.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HighlightedText extends StatelessWidget {
+  final String text;
+  final String highlight;
+  final Color highlightColor;
+  final Color textColor;
+  final double fontSize;
+  const HighlightedText(
+      {Key key,
+      @required this.text,
+      @required this.highlight,
+      @required this.highlightColor,
+      this.textColor = Colors.white,
+      this.fontSize = 18})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(color: textColor, fontSize: fontSize);
+    if (highlight.isEmpty ||
+        !text.contains(new RegExp(highlight, caseSensitive: false))) {
+      return RichText(
+        text: TextSpan(text: text, style: textStyle),
+      );
+    }
+    final processedPart = text.splitMapJoin(
+        new RegExp(highlight, caseSensitive: false),
+        onMatch: (m) => "*${m.group(0)}*");
+    final parts = processedPart.split("*");
+    print(parts);
+    return RichText(
+      text: TextSpan(
+          children: parts
+              .map((part) => TextSpan(
+                  text: part,
+                  style: textStyle.copyWith(
+                      color: part.contains(
+                              new RegExp(highlight, caseSensitive: false))
+                          ? highlightColor
+                          : textColor)))
+              .toList()),
+    );
+  }
+}
